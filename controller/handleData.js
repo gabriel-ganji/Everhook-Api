c2f96fc658ef910e80eaf7cd5ba918d66bfc94e5
Gabrielganji <gabrielganji01011010@gmail.com>
2022-09-09 15:49:02 -0300

    formValues

--- controller/handleData.js
+++ controller/handleData.js
@@ -3,12 +3,13 @@ const time = require("./timePost");
 const saveOnMongo = require("../middleware/saveOnMongo");
 
 const dataReq = function (uuid, req, typeRequest) {
-    
+    console.log(req);
     const fullRequest = req;
 
     const webhookRequest = {
         header: {},
-        body: {}
+        body: {},
+        form_values: {}
     }
     
     const header = new Object();
@@ -21,17 +22,45 @@ const dataReq = function (uuid, req, typeRequest) {
     header["date"] = time();
     header["Type_Request"] = typeRequest;
     webhookRequest.header = header;
+    console.log("req body", Object.keys(req.body).length);
 
-    if (req.body == undefined) {
+    if (Object.keys(req.body).length == 0) {
         console.log('Estamos em if');
         webhookRequest.body = "";
+        webhookRequest.form_values = "";
         
     } else {
         console.log('Estamos em else');
         webhookRequest.body = req.body;
-    }
+        const array = [];
+        
+        let formValue = Object.values(req.body)[0];
+        formValue = formValue.split("&");
     
-    console.log(req.body);
+        for (i of formValue) {
+            const a = i.split("=");
+            array.push(a);
+        }
+
+        const formValueEnd = {}
+
+        for (let i = 0; i < array.length; i++) {
+            
+            if (i == 0) {
+                formValueEnd["hottok"] = array[i][0];
+            } else {
+            
+                let a = array[i];
+                
+                if (a[1] == '') {
+                    formValueEnd[a[0]] = 'empty';
+                } else {
+                    formValueEnd[a[0]] = a[1];
+                }
+            }
+        }
+        webhookRequest.form_values = formValueEnd;
+    }
 
     const status = saveOnMongo(webhookRequest);
     return status;
